3877ad7a86bbcff922eb4c6f5a5edadd96c5abe1
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-07-19 22:42:56 +0900

    move test

---
+++ test/wda_client/status_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+require 'webmock/minitest'
+require 'json'
+
+class WdaClient::StatusTest < Minitest::Test
+  def test_get_status
+    base_host = 'localhost:8100'
+
+    json =<<-EXPECTED_JSON
+  {
+  "value" : {
+    "state" : "success",
+    "os" : {
+      "name" : "iPhone OS",
+      "version" : "9.3"
+    },
+    "ios" : {
+      "simulatorVersion" : "9.3"
+    },
+    "build" : {
+      "time" : "Jul 18 2016 17:28:56"
+    }
+  },
+  "sessionId" : null,
+  "status" : 0
+  }
+    EXPECTED_JSON
+
+    stub_request(:get, "#{base_host}/status")
+      .with(headers:{ 'Content-Type' => 'application/json' })
+      .to_return(body: json)
+
+    client = ::WdaClient.new caps: "dummy caps"
+    res_json_body = client.get_status
+
+    assert_equal JSON.parse(json), res_json_body
+  end
+end
